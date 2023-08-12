69f9af8a4180dec87b1169b5c31a492405f52763
Alex Jercan <jercan_alex27@yahoo.com>
2023-08-12 12:59:25 +0300

    basic

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
-	import { SignedIn } from "sveltefire";
+	import { SignedIn, Doc, Collection } from 'sveltefire';
 
-	import SignOut from "$lib/components/SignOut.svelte";
+	import SignOut from '$lib/components/SignOut.svelte';
 </script>
 
 <SignedIn let:user>
 	<h1>Account</h1>
 	<p>Logged in as {user.displayName}</p>
+
+	<Doc ref={`tracker/${user.uid}`} let:ref>
+		<h2>Habits</h2>
+
+        <Collection ref={ref?.path + "/habits"} let:data={habits}>
+            <ul>
+                {#each habits as habit}
+                    <li>{habit.name}</li>
+                {/each}
+            </ul>
+        </Collection>
+	</Doc>
+
 	<SignOut />
 </SignedIn>

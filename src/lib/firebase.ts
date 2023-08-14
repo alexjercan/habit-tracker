63160aa62534a1faa1741845481bc2654e79c5f3
Alex Jercan <jercan_alex27@yahoo.com>
2023-08-14 09:21:45 +0300

    meh

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,13 +1,3 @@
-import {
-	PUBLIC_FIREBASE_API_KEY,
-	PUBLIC_FIREBASE_AUTH_DOMAIN,
-	PUBLIC_FIREBASE_PROJECT_ID,
-	PUBLIC_FIREBASE_STORAGE_BUCKET,
-	PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	PUBLIC_FIREBASE_APP_ID,
-	PUBLIC_FIREBASE_MEASUREMENT_ID
-} from "$env/static/public";
-
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
@@ -20,13 +10,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: PUBLIC_FIREBASE_API_KEY,
-	authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: PUBLIC_FIREBASE_APP_ID,
-	measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
+	apiKey: "AIzaSyBRsHYT6FVBwG_CZFqvHGgkdRE3jkM9mhM",
+	authDomain: "habittacker.firebaseapp.com",
+	projectId: "habittacker",
+	storageBucket: "habittacker.appspot.com",
+	messagingSenderId: "584678627298",
+	appId: "1:584678627298:web:859b4ce0c707a6075b8bbb",
+	measurementId: "G-D4KHZM016L",
 };
 
 const app = initializeApp(firebaseConfig);

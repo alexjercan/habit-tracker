168f2047764805947ae3ab04aada5d7618dc007d
Alex Jercan <jercan_alex27@yahoo.com>
2023-08-16 15:33:01 +0300

    fix: seo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,15 @@
 	import HabitTracker from "$lib/components/HabitTracker.svelte";
 </script>
 
+<svelte:head>
+	<title>Habit Tracker</title>
+	<meta
+		name="description"
+        content="Dashboard that allows users to visualize and update the status
+        of their daily habits."
+	/>
+</svelte:head>
+
 <SignedIn let:user>
 	<HabitTracker {user} {firestore} />
 </SignedIn>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -4,6 +4,15 @@
 	import { firestore } from "$lib/firebase";
 </script>
 
+<svelte:head>
+	<title>Account</title>
+	<meta
+		name="description"
+        content="Page that allows user to manage their accounts. This page lets
+        users add, edit, and delete daily habits."
+	/>
+</svelte:head>
+
 <SignedIn let:user>
 	<Account {user} {firestore} />
 </SignedIn>
